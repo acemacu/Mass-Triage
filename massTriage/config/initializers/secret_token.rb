82254079f5f5d7aa7d6311e6891c0ef256a0e04a
Ana Martinez <anacecilia.martinez@sv.cmu.edu>
2011-10-09 22:27:37 -0700

    Adding project of ruby on rails

---
+++ massTriage/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MassTriage::Application.config.secret_token = '65ecbe91e3583f75d826e3fd8ae4c624c5c70d44f77edcbba45927d15062c8dacdf907f13cea201557cba786ee8f808bfa3d5160563ede2e432b3eb660e2f18b'
