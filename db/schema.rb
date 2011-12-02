4f16904eeb1c00be0a093eb42404047b7c51394d
Ana Martinez <anacecilia.martinez@sv.cmu.edu>
2011-12-01 18:12:32 -0800

    Added logout button Fixed dispatcher shouldn't see delete for patients

--- db/schema.rb
+++ db/schema.rb
@@ -1,7 +1,3 @@
-# For more information on the technology stack selected, please refer to the document "Technology feasibility analysis"
-# Developed by: Carnegie Mellon University - Team Triage
-# Copyright:    Field Applications
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
