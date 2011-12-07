ba50845d8f6d3eed14716c47c8c4fac90ddb49e7
Ana Martinez <anacecilia.martinez@sv.cmu.edu>
2011-12-07 03:21:18 -0800

    Fixed the number of patients counted without is_deleted

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -17,21 +17,23 @@ class Incident < ActiveRecord::Base
   validates_presence_of :date, :on => :create, :message => "can't be blank"
 
 
-  def color_dist(color)
-   patients.find_all_by_tagColor(color).count
+  def color_dist(color, id)
+   patients_untrasported = Patient.find :all, :conditions => ['tagColor = ? AND is_deleted = ? AND incident_id = ?', color, false, id]
+   patients_untrasported.count
   end
   
   def amb_count(status)
     ambulances.find_all_by_status(status).count
   end
   
-  def untransported(status)
-    if patients.find_all_by_status(status).count == 0
+  def untransported(status, id)
+    patients_untrasported = Patient.find :all, :conditions => ['status = ? AND is_deleted = ? AND incident_id = ?', status, false, id]
+    if patients_untrasported.count.to_s == 0
       return "There are no untransported patients listed at the incident"
-    elsif patients.find_all_by_status(status).count == 1
+    elsif patients_untrasported.count.to_s == 1
       return "There is currently 1 untransported patient listed at the incident"
     else
-      return "There are currently " << patients.find_all_by_status(status).count.to_s() << " untransported patients listed at the incident"
+      return "There are currently " << patients_untrasported.count.to_s << " untransported patients listed at the incident"
     end
        
   end
